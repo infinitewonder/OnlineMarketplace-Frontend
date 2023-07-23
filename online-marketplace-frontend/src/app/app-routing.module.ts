4e554abeac801888a5132833a5c9485f40a0eea7
infinitewonder <98650402+infinitewonder@users.noreply.github.com>
2023-07-24 06:14:52 +0800

    Routing

--- online-marketplace-frontend/src/app/app-routing.module.ts
+++ online-marketplace-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './path/to/login/login.component';
+import { GameComponent } from './path/to/game/game.component';
+import { LeaderboardComponent } from './path/to/leaderboard/leaderboard.component';
+import { StatsComponent } from './path/to/stats/stats.component';
+
 const routes: Routes = [
-  //   { path: '', component: LoginComponent },
-  //   { path: 'game', component: GameComponent },
-  //   { path: 'leaderboard', component: LeaderboardComponent },
-  //   { path: 'stats', component: StatsComponent },
+  { path: '', component: LoginComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'stats', component: StatsComponent },
 ];
 
 @NgModule({
--- online-marketplace-frontend/src/app/game/game.component.ts
+++ online-marketplace-frontend/src/app/game/game.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as Phaser from 'phaser';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit, OnDestroy {
+  game: Phaser.Game;
+  scene: Phaser.Scene;
+  item: Phaser.Physics.Arcade.Image;
 
+  constructor() {}
+
+  ngOnInit() {
+    const config: Phaser.Types.Core.GameConfig = {
+      title: 'Sample',
+      parent: 'gameContainer',
+      width: 800,
+      height: 600,
+      type: Phaser.AUTO,
+      scene: {
+        preload: this.preload,
+        create: this.create,
+        update: this.update,
+      },
+    };
+
+    this.game = new Phaser.Game(config);
+  }
+
+  preload(): void {
+    this.load.image('item', 'assets//item.png'); // Replace item.png with the path to your own asset.
+  }
+
+  create(): void {
+    // Create item at random position
+    this.item = this.physics.add.image(
+      Phaser.Math.Between(0, 800),
+      Phaser.Math.Between(0, 600),
+      'item'
+    );
+    this.item.setInteractive();
+
+    // Register click event
+    this.item.on('pointerdown', (pointer) => {
+      // Increase score, POST to backend, etc. Handle it here.
+    });
+  }
+
+  update(): void {
+    // Move item to random position
+    this.item.x = Phaser.Math.Between(0, 800);
+    this.item.y = Phaser.Math.Between(0, 600);
+  }
+
+  ngOnDestroy() {
+    this.game.destroy(true);
+  }
 }
