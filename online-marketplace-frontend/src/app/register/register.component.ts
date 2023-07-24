34a36d33484a88240cabdf014117955fe7695caf
infinitewonder <98650402+infinitewonder@users.noreply.github.com>
2023-07-25 06:04:33 +0800

    Register

--- online-marketplace-frontend/src/app/register/register.component.ts
+++ online-marketplace-frontend/src/app/register/register.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.userService.register(this.registerForm.value).subscribe({
+        next: (res) => {
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          // Handle error here
+        },
+        complete: () => {
+          // Handle completion here
+        },
+      });
+    } else {
+      // handle form validation errors
+    }
+  }
 }
