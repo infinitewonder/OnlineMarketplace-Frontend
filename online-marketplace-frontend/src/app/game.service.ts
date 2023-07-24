a5be24d9ba2c70d2db04357934f27c47e3bb3305
infinitewonder <98650402+infinitewonder@users.noreply.github.com>
2023-07-24 09:10:15 +0800

    Update game.service.ts

--- online-marketplace-frontend/src/app/game.service.ts
+++ online-marketplace-frontend/src/app/game.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root',
-})
 export class GameService {
   private url = '/api/game';
   private _score: number = 0;
