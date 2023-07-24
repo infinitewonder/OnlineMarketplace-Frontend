321bf945b28e3b677f491e7a4bf317a02c6ebf14
infinitewonder <98650402+infinitewonder@users.noreply.github.com>
2023-07-25 00:09:04 +0800

    Update app.module.ts

--- online-marketplace-frontend/src/app/app.module.ts
+++ online-marketplace-frontend/src/app/app.module.ts
@@ -46,8 +46,6 @@ import { ProfileComponent } from './profile/profile.component';
     MatTableModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
     NgxWebstorageModule.forRoot(),
