186cb477caeff0a93ee561d862b17038bef48b1a
infinitewonder <98650402+infinitewonder@users.noreply.github.com>
2023-07-25 02:35:37 +0800

    Update user.service.ts

--- online-marketplace-frontend/src/app/user.service.ts
+++ online-marketplace-frontend/src/app/user.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Subject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 import { UserStore, UserState, User } from './store/user.store';
+import { GameService } from './game.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,11 @@ export class UserService {
   private baseUrl = 'https://onlinemarketplace-production.up.railway.app/users';
   private loggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor(private userStore: UserStore, private http: HttpClient) {}
+  constructor(
+    private userStore: UserStore,
+    private gameService: GameService,
+    private http: HttpClient
+  ) {}
 
   login(credentials: any) {
     return this.http.post(`${this.baseUrl}/login`, credentials).pipe(
@@ -20,6 +25,7 @@ export class UserService {
         if (res) {
           this.userStore.update({ user: res.user });
           this.loggedIn.next(true);
+          this.gameService.resetScore();
         }
       })
     );
